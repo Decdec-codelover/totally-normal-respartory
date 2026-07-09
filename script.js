a5387aeee2b440b3a10e77c8bdbeaa7df0fd1a78
Declan Pangburn <Declan.pangburn@icloud.com>
2026-07-09 11:38:05 -0400

    Add prankLoop function for infinite alert loop

---
+++ script.js
@@ -0,0 +1,7 @@
+// Triggers the "67 virus" infinite prank loops
+function prankLoop() {
+    alert("Warning: Your device has detected 67 viruses!");
+    while(true) {
+        alert("April Fools! Click OK to try escaping.");
+    }
+}
